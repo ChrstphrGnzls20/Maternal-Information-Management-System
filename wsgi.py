84650094d9e3f5befd2d4b5cb5b47b29dc639b5c
Christopher <cgonzalesmain@gmail.com>
2023-02-16 16:32:54 +0800

    testing autodeployment

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,11 @@
 from application import init_app
 
-def appEntryPoint(environment="development"):
-    app = init_app(environment)
-    app.run()
-
-if __name__ == "__main__":
-    appEntryPoint()
+app = init_app("deployment")
     
 
+if __name__ == "__main__":
+    app.run()
+    
+# HELLO EXAMPLE PUSH!
 # TO RUN THE APPLICATION UNDER VIRTUALENV RUN 
 # source "c:/Users/Christopher/Desktop/Caps_1_System/env/Scripts/activate"
