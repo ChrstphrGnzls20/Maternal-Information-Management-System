313b7111816cb24f5490345cabe704c927b88fec
Christopher <cgonzalesmain@gmail.com>
2023-02-16 19:17:26 +0800

    testing autodeply

--- application/__init__.py
+++ application/__init__.py
@@ -86,6 +86,7 @@ def init_app(environment):
                 return '', 200
             else:
                 return '', 400
+            #test
     # @app.route("/git_update", methods=["POST"])
     # def updateProject():
     #     import git, os
